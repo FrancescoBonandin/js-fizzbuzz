a91cc395b3127effda8395d6c2a46993039bdbd1
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-06-14 14:20:07 +0200

    preparata base

---
+++ js/my-script.js
@@ -0,0 +1,16 @@
+// dichiarazione variabili utili
+
+const fizz = "FIZZ";
+
+const buzz = "BUZZ";
+
+const fizzBuzz = fizz + buzz;
+
+
+let i;
+
+for(i=0;i <= 100; i++){
+
+    console.log("i", i, typeof i)
+    
+}
